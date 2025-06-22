9382a8ba267968c6396ec41840136ee6477cebfe
mariyarahael <mariyarahael@gmail.com>
2025-06-22 14:31:19 +0530

    new

--- model/complaint.js
+++ model/complaint.js
@@ -1,9 +1,20 @@
 var mongoose = require('mongoose')
 // schema creation
 var complaintschema = mongoose.Schema({
+    userId: {
+     type: mongoose.Schema.Types.ObjectId,
+     ref: 'user', // important for population
+     required: true
+    },
     date:Date,
     issue:String,
-    description:String    
+    description:String  ,
+    status: {
+    type: String,
+    enum: ['pending', 'responded'],
+    default: 'pending'
+    }
+
 });
 // model creation
 var complaintmodel = mongoose.model("complaint",complaintschema);
